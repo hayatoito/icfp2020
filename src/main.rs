65aacc0c52c72faed0f46bb386e7efb7bb73f8ba
Hayato Ito <hayato@google.com>
2020-07-18 12:09:21 +0900

    Test requests to the proxy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
 use reqwest::StatusCode;
-use std::io::Read;
 
 type Result<T> = anyhow::Result<T>;
+
+// https://icfpc2020-api.testkontur.ru/swagger/index.html
+
+fn get(server_url: &str, player_key: &str, api: &str) -> Result<()> {
+    let url = format!("{}{}?apikey={}", server_url, api, player_key);
+    println!("get url: {}", url);
+    let response = reqwest::blocking::get(&url)?;
+    match response.status() {
+        StatusCode::OK => {
+            println!("{}", response.text()?);
+        }
+        _ => {
+            println!("Unexpected server response:");
+            println!("HTTP code: {}", response.status());
+            println!("{}", response.text()?);
+        }
+    }
+    Ok(())
+}
+
+fn post(server_url: &str, player_key: &str, api: &str, body: String) -> Result<()> {
+    let url = format!("{}/{}?apikey={}", server_url, api, player_key);
+    println!("post url: {}, body: {}", url, body);
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.post(&url).body(body).send()?;
+    match response.status() {
+        StatusCode::OK => {
+            println!("{}", response.text()?);
+        }
+        _ => {
+            println!("Unexpected server response:");
+            println!("HTTP code: {}", response.status());
+            println!("{}", response.text()?);
+        }
+    }
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
@@ -13,24 +50,9 @@ fn main() -> Result<()> {
 
     println!("ServerUrl: {}; PlayerKey: {}", server_url, player_key);
 
-    let client = reqwest::blocking::Client::new();
-    let mut response = client.post(server_url).body(player_key.clone()).send()?;
-
-    match response.status() {
-        StatusCode::OK => {
-            let mut content = String::new();
-            response.read_to_string(&mut content)?;
-            println!("{}", content);
-        }
-        _ => {
-            println!("Unexpected server response:");
-            println!("HTTP code: {}", response.status());
-            let mut content = String::new();
-            response.read_to_string(&mut content)?;
-            println!("{}", content);
-            std::process::exit(2);
-        }
-    }
+    get(server_url, player_key, "/submissions")?;
+    get(server_url, player_key, "/teams/current")?;
+    post(server_url, player_key, "/alians/send", "0".to_string())?;
 
     Ok(())
 }
