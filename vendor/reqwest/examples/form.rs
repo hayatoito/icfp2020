6ceb6a7152aaa5a232fd8ced265058e0df5c12bc
Hayato Ito <hayato@google.com>
2020-07-21 15:50:35 +0900

    Remove vendor

--- vendor/reqwest/examples/form.rs
+++
@@ -1,9 +0,0 @@
-#[tokio::main]
-async fn main() {
-    reqwest::Client::new()
-        .post("http://www.baidu.com")
-        .form(&[("one", "1")])
-        .send()
-        .await
-        .unwrap();
-}
